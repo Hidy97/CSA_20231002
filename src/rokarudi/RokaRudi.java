98934174ae63961ac1a3196a5c313efe2a6e7a19
Hidy97 <csaladi.alexandra@gmail.com>
2023-10-02 09:01:05 +0200

    feltolt() és kever() készítése

--- src/rokarudi/RokaRudi.java
+++ src/rokarudi/RokaRudi.java
@@ -1,20 +1,61 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package rokarudi;
 
-/**
- *
- * @author csaladi.alexandra
- */
+import java.util.Scanner;
+
 public class RokaRudi {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static String[] pakli = new String[22];
+    private static Scanner sc = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        feltolt();
+        megjelenit();
+        int oszlop = sc.nextInt();
+        kever(oszlop);
+        ezVolt();
+    }
+
+    private static void feltolt() {
+        String[] szin = {"P", "T", "M", "S"};
+        String[] ertek = {"Á", "K", "J", "IX", "X", "VIII"};
+        
+        for (String sz : szin) {
+            for (int e = 0; e < ertek.length && e < pakli.length; e++) {
+                pakli = szin + "_" + e;
+            }
+        }
+        
+    }
+
+    private static void megjelenit() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    private static void kever(oszlop) {
+        String[] ujPakli = new String[22];
+        
+        switch (oszlop) {
+            case 1:
+                for (int i = 0; i < 7; i++) {
+                    ujPakli[i] = 20-(i-1)*3;
+                    ujPakli[i+7] = 19-(i-1)*3;
+                    ujPakli[i+14] = 21-(i-1)*3;
+                }
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            default:
+                throw new AssertionError();
+        }
+        pakli = ujPakli;
+    }
+
+    private static void ezVolt() {
+        System.out.println("Ez volt az a lap: " + pakli[11] + "?");
     }
     
 }
