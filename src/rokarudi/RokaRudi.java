f1534142975faf5d8140f8d49895aba9e6adf297
Hidy97 <csaladi.alexandra@gmail.com>
2023-10-02 09:08:05 +0200

    kever() metódus javítása

--- src/rokarudi/RokaRudi.java
+++ src/rokarudi/RokaRudi.java
@@ -6,7 +6,7 @@ public class RokaRudi {
 
     private static String[] pakli = new String[22];
     private static Scanner sc = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
         feltolt();
         megjelenit();
@@ -18,44 +18,60 @@ public class RokaRudi {
     private static void feltolt() {
         String[] szin = {"P", "T", "M", "S"};
         String[] ertek = {"Á", "K", "J", "IX", "X", "VIII"};
-        
+
         for (String sz : szin) {
             for (int e = 0; e < ertek.length && e < pakli.length; e++) {
-                pakli = szin + "_" + e;
+                //pakli = szin + "_" + e;
             }
         }
-        
+
     }
 
     private static void megjelenit() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        for (int i = 0; i < pakli.length; i++) {
+            System.out.println(pakli[i]);
+            if (i % 3 == 0) {
+                System.out.println("");
+            }
+        }
     }
-    
-    private static void kever(oszlop) {
+
+
+
+    private static void ezVolt() {
+        System.out.println("Ez volt az a lap: " + pakli[11] + "?");
+    }
+
+    private static void kever(int oszlop) {
         String[] ujPakli = new String[22];
         
         switch (oszlop) {
             case 1:
                 for (int i = 0; i < 7; i++) {
-                    ujPakli[i] = 20-(i-1)*3;
-                    ujPakli[i+7] = 19-(i-1)*3;
-                    ujPakli[i+14] = 21-(i-1)*3;
+                    ujPakli[i] = 20 - (i - 1) * 3;
+                    ujPakli[i + 7] = 19 - (i - 1) * 3;
+                    ujPakli[i + 14] = 21 - (i - 1) * 3;
                 }
                 break;
             case 2:
-                
+                for (int i = 0; i < 7; i++) {
+                    ujPakli[i] = 20 - (i - 1) * 3;
+                    ujPakli[i + 7] = 19 - (i - 1) * 3;
+                    ujPakli[i + 14] = 21 - (i - 1) * 3;
+                }
                 break;
             case 3:
-                
+                for (int i = 0; i < 7; i++) {
+                    ujPakli[i] = 20 - (i - 1) * 3;
+                    ujPakli[i + 7] = 19 - (i - 1) * 3;
+                    ujPakli[i + 14] = 21 - (i - 1) * 3;
+                }
                 break;
             default:
                 throw new AssertionError();
         }
+        
         pakli = ujPakli;
     }
 
-    private static void ezVolt() {
-        System.out.println("Ez volt az a lap: " + pakli[11] + "?");
-    }
-    
 }
